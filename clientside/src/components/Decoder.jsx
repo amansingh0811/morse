0348aa7065175b7943cd50b62c62f4342d0f0ea3
Aman Singh <97546940+amansingh0811@users.noreply.github.com>
2023-10-22 11:42:54 +0530

    Updated Decoder.jsx

--- clientside/src/components/Decoder.jsx
+++ clientside/src/components/Decoder.jsx
@@ -1,38 +1,45 @@
 // src/components/Decoder.js
 import React, { useState } from 'react';
-import axios from 'axios';
+import { morseToText } from '../morse-code-api';
+import './Decoder.css'; // Import the CSS file for styling
 
 const Decoder = () => {
-  const [morseCode, setMorseCode] = useState('');
-  const [decodedResult, setDecodedResult] = useState('');
+  const [inputMorse, setInputMorse] = useState('');
+  const [decodedText, setDecodedText] = useState('');
 
-  const handleDecode = async () => {
-    try {
-      const response = await axios.post('http://localhost:5000/decode', { morseCode });
-      setDecodedResult(response.data.text);
-    } catch (error) {
-      console.error('Error decoding Morse code:', error);
-    }
+  const handleDecode = () => {
+    const decodedTextResult = morseToText(inputMorse);
+    setDecodedText(decodedTextResult);
+  };
+
+  const handleCopyToClipboard = () => {
+    const textArea = document.createElement('textarea');
+    textArea.value = decodedText;
+    document.body.appendChild(textArea);
+    textArea.select();
+    document.execCommand('copy');
+    document.body.removeChild(textArea);
   };
 
   return (
-    <div style={{ textAlign: 'center', padding: '20px', backgroundColor: '#f0f8ff' }}>
-      <h2 style={{ color: '#3333cc' }}>Morse Code Decoder</h2>
+    <div className="decoder-container">
+      <h1>Morse Code Decoder</h1>
       <textarea
-        rows="4"
-        cols="50"
-        placeholder="Enter Morse code to decode..."
-        value={morseCode}
-        onChange={(e) => setMorseCode(e.target.value)}
-        style={{ margin: '10px', padding: '10px' }}
+        className="decoder-textarea"
+        placeholder="Enter Morse code to decode"
+        value={inputMorse}
+        onChange={(e) => setInputMorse(e.target.value)}
       />
-      <br />
-      <button onClick={handleDecode} style={{ backgroundColor: '#FF6347', color: 'white', padding: '10px 20px', border: 'none', borderRadius: '5px', cursor: 'pointer' }}>
+      <button className="decoder-button" onClick={handleDecode}>
         Decode
       </button>
-      <br />
-      <strong style={{ color: '#3333cc' }}>Decoded Result:</strong>
-      <p style={{ backgroundColor: '#e6f7ff', padding: '10px', borderRadius: '5px' }}>{decodedResult}</p>
+      <div className="result-container">
+        <p>Decoded Text:</p>
+        <p className="decoded-text">{decodedText}</p>
+        <button className="copy-button" onClick={handleCopyToClipboard}>
+          Copy to Clipboard
+        </button>
+      </div>
     </div>
   );
 };
